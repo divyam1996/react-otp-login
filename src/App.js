5736353d54f4fa8c0ba383c26c537c42c64dc59d
Divyam Bansal <divyambansal17@gmail.com>
2024-03-11 23:12:13 +0530

    stopwatch also added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import PhoneOtpForm from './components/phone-login';
+import StopWatch from './components/StopWatch/StopWatch.js';
 
 function App() {
   return (
     <div className='App'>
         <h1>Login with phone</h1>
         <PhoneOtpForm/>
+        <StopWatch />
     </div>
   );
 }
